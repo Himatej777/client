dafb0361ec7496a70309a1ae427c0f8c942aed72
Himatej SRY <evilofhimatejdora@gmail.com>
2021-08-03 14:04:16 +0530

    Removed unwanted boiler plate files

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React , {useEffect,useRef} from 'react'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
 
 export default function TextEditor() {
+    
+    const wrapperRef = useRef();
+    
+    useEffect(()=>{
+        wrapperRef.current.innerHTML = "";
+        const editor = document.createElement('div');
+        wrapperRef.current.append(editor);
+        new Quill(editor, { theme : "snow" }); 
+    },[]);
+
     return (
-        <div>
-          TextEditor  
-        </div>
+      <div id="container" ref={wrapperRef}></div>  
     )
-}
+} 
